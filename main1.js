00059c027a6ed383157e5f5ff32cbeeeea22877a
Rajesh7889 <khajuriarajesh768@gmail.com>
2023-05-22 09:14:12 +0530

    deletetion

--- main1.js
+++ main1.js
@@ -12,6 +12,7 @@
    let net=document.getElementById('balance');
    let netin=document.getElementById('balance1');
    let netout=document.getElementById('balance2');
+   let now=new Date();
    function innput(){
     out1.disabled=true;
     title.style.visibility='visible';
@@ -30,16 +31,26 @@
     title.style.visibility='hidden';
     in2.style.display='none';
     in1.style.display='block';
+    if((title.value =='')){
+        alert('please enter a valid number');
+        return 0;
+      }
     saving(a);
     display();
+    title.value='';
    }
    function outtput1(a){
     in1.disabled=false;
     title.style.visibility='hidden';
     out2.style.display='none';
     out1.style.display='block';
+    if((title.value =='')){
+        alert('please enter a valid number');
+        return 0;
+      }
     saving(a);
     display();
+    title.value='';
    }
    
    function saving(a){
@@ -59,6 +70,7 @@
              let retrivedD=JSON.parse(localStorage.getItem(`${retrived[index].book}`));
               total=retrivedD.shift();
               console.log(title.value);
+              
               if(a){
                  total.netBalance= parseInt(total.netBalance)+parseInt(title.value);
                  total.totalIn=parseInt(total.totalIn)+parseInt(title.value);
@@ -69,6 +81,17 @@
               taskobj.unshift(total);
          localStorage.setItem(`${retrived[index].book}`,JSON.stringify(taskobj));       
    }
+   function deletee(a){
+    let webtask = JSON.parse(localStorage.getItem(`${retrived[index].book}`));
+    let opt =confirm("this action can't be undo");
+    if(opt){
+      webtask.splice(a,1);
+   localStorage.setItem(`${retrived[index].book}`,JSON.stringify(webtask));
+   display();
+    }else {
+   return 0;
+    }
+}
    
    function display(){
     let webtask = JSON.parse(localStorage.getItem(`${retrived[index].book}`));
@@ -78,13 +101,19 @@
     let create=document.querySelectorAll('#book-record');
     let table= `<tr><th></th><tr>`;
     create[0].innerHTML=table;
-    for(let i=1;i<=webtask.length;i++){
-    table+=`<tr><td> <div id='book-box'>
-                    <div id="books"><div id='bok' onclick="edit(${i})"> Net Balance:${webtask[i].netBalance}</div>
+    for(let i=0;i<webtask.length-1;i++){
+    table+=`<tr><td> <div id='book-box'><div>${now.toLocaleString()}</div>
+                    <div id="books">
+                    <div>
+                    <div id='bok1' onclick="edit(${i})"> Net Balance:${webtask[i].netBalance}</div>
+                    <div id='bok1' onclick="edit(${i})"> last in:${webtask[i].totalIn - webtask[i+1].totalIn}</div>
+                    <div id='bok1' onclick="edit(${i})"> last out:${webtask[i].totalOut - webtask[i+1].totalOut}</div>
+                    </div>
                     <div class='edt' onclick='edit(${i})'><img id="edit" src='edit.jpg'></div>
                     <div class='edt' onclick='deletee(${i})'><img id='delete' src='delete.jpg'></div>
                     </div></div>
                     </td></tr>`;
     create[0].innerHTML=table;
     }
+    
 }}
